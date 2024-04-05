0cd0df51c97ed942fc4b0a9a7b94b3c1ff0789d8
rodrigonalo <rodrigo.gonzalez.alonso@gmail.com>
2024-04-05 15:59:17 +0200

    update

--- scripts/01_datacleaning.R
+++ scripts/01_datacleaning.R
@@ -5,4 +5,4 @@ library(readxl)
 df_kanton <- read_excel("C:/Users/Victor/Desktop/ML1/data/data_kantons.xlsx")
 df_homegate <- read_excel("C:/Users/Victor/Desktop/ML1/data/data_homegate.xlsx")
 
-fdsfisubfs
+fdsfisubfshvjhvjhv
