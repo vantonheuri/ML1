a4d28465c1afcbec410752483312ac21ae1477b2
vantonheuri <123073572+vantonheuri@users.noreply.github.com>
2024-04-05 15:57:30 +0200

    update 01_datacleaning

---
+++ scripts/01_datacleaning.R
@@ -0,0 +1,8 @@
+install.packages("readxl")
+library(readxl)
+
+# Load datasets
+df_kanton <- read_excel("C:/Users/Victor/Desktop/ML1/data/data_kantons.xlsx")
+df_homegate <- read_excel("C:/Users/Victor/Desktop/ML1/data/data_homegate.xlsx")
+
+fdsfisubfs
