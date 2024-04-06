76af63ceddc266eab8107b8b1279ad02a1b0bb61
vantonheuri <123073572+vantonheuri@users.noreply.github.com>
2024-04-06 11:06:57 +0200

    update_data_cleaned

--- scripts/02_ExploratoryDataAnalysis.R
+++ scripts/02_ExploratoryDataAnalysis.R
@@ -1,10 +1,10 @@
 --------------------------------------------------------------------------------
   
-# Data Cleaning
+# Exploratory Data Analysis
 
 --------------------------------------------------------------------------------
 # Install libraries
-  
+
 library(readr)
 library(ggplot2)
 library(stringr)
@@ -12,31 +12,10 @@ library(dplyr)
 library(readxl)
 
 --------------------------------------------------------------------------------
-
+  
 # Load data sets
 df_kanton <- read_excel("C:/Users/Victor/Desktop/ML1/data/data_raw/data_kantons.xlsx")
 df_homegate <- read_excel("C:/Users/Victor/Desktop/ML1/data/data_raw/dataset_homegate.xlsx")
 
 head(df_kanton)
 head(df_homegate)
-
-colnames(df_kanton)
-colnames(df_homegate)
-
-# Left Join
-df_total <- merge(df_homegate,df_kanton, by = "Canton")
-
-head(df_total)
-colnames(df_total)
-dim(df_total)
-
-# > dim(df_total)
-# [1] 3417   23
-
-
-# Now we focus only on analysing the properties to be rented and whether they are houses or appartments
-df_total <- df_total[df_total$Type == "Rent" & (df_total$Category == "House" | df_total$Category == "Appartment"), ]
-
-dim(df_total)
-# > dim(df_total)
-# [1] 2141   23
