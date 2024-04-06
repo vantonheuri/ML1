b74d0358adf181532a6d05e38814ebb9b619b566
vantonheuri <123073572+vantonheuri@users.noreply.github.com>
2024-04-06 10:24:17 +0200

    update

--- scripts/01_datacleaning.R
+++ scripts/01_datacleaning.R
@@ -1,8 +1,29 @@
-install.packages("readxl")
-library(readxl)
+--------------------------------------------------------------------------------
+  
+# Data Cleaning
 
-# Load datasets
-df_kanton <- read_excel("C:/Users/Victor/Desktop/ML1/data/data_kantons.xlsx")
-df_homegate <- read_excel("C:/Users/Victor/Desktop/ML1/data/data_homegate.xlsx")
+--------------------------------------------------------------------------------
+# Install libraries
+library(readr)
+library(ggplot2)
+library(stringr)
 
-fdsfisubfshvjhvjhv
+--------------------------------------------------------------------------------
+
+# Load data sets
+df_kanton <- read_csv("C:/Users/Victor/Desktop/ML1/data/data_kantons.csv")
+df_homegate <- read_csv("C:/Users/Victor/Desktop/ML1/data/dataset_homegate.csv")
+
+head(df_kanton)
+head(df_homegate)
+
+colnames(df_kanton)
+colnames(df_homegate)
+
+# Dividir la columna en df_kanton utilizando el delimitador ;
+df_kanton <- str_split_fixed(df_kanton, ";", n = 12)
+
+
+# Left Join
+merged_df <- merge(df_homegate, df_kanton, by.x = "Code", by.y = "GEO_KANTON", all.x = TRUE)
+
