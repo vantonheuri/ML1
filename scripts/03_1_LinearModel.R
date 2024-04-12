d823d07a3d5532a464d86613a56dc463a308d24a
cgleonr <carlos.leon@stud.hslu.ch>
2024-04-12 14:05:27 +0200

    2 linear models ready, working on model analysis

--- scripts/03_1_LinearModel.R
+++ scripts/03_1_LinearModel.R
@@ -5,7 +5,7 @@
 
 # Reading in data
 enc_data <- read.csv("data/data_cleaned/data_total_model.csv")
-
+str(enc_data)
 #Splitting the data into training and testing
 split_ratio <- 0.8
 training_indices <- sample(1:nrow(enc_data), 
@@ -17,7 +17,56 @@ test_set <- enc_data[-training_indices,]
 
 lm_model <- lm(Price_Gross ~ ., data=train_set[-train_set$Price_Gross])
 
+#Evaluation
+summary(lm_model)
+summary(lm_model)$r.squared
+summary(lm_model)$adj.r.squared
+summary(lm_model)$accuracy
+
+# Retraining with only relevant variables
+rel_data <- enc_data[, !(names(enc_data) %in% 
+                           c("Package_Product_num",
+                             "Type_num",
+                             "GDP_2020_21","GDP_per",
+                             "Population",
+                              "Area_km2", "Density")), drop = TRUE]
+training_indices <- sample(1:nrow(rel_data), 
+                           size=nrow(rel_data) * split_ratio,
+                           replace=FALSE)
+
+rel_train_set <- rel_data[training_indices,]
+rel_test_set <- rel_data[-training_indices,]
+
+new_lm_model <- lm(Price_Gross ~ ., data=rel_train_set[-rel_train_set$Price_Gross])
+
+summary(lm_model)
+summary(new_lm_model) #Similar performance on less variables, prevent overfitting
+
 #Model Predictions
 lm_predictions <- predict(lm_model, newdata=test_set[-test_set$Price_Gross])
-summary(lm_predictions)
-#Evaluation
+new_lm_predictions <- predict(new_lm_model, 
+                              newdata=rel_test_set[-rel_test_set$Price_Gross])
+
+#Model Evaluation
+
+par(mfrow=c(2,2))
+plot(lm_model)
+plot(new_lm_model)
+
+par(mfrow=c(1,2))
+plot(test_set$Price_Gross, lm_predictions, main="Target Vs. lm_predictions")
+plot(rel_test_set$Price_Gross, new_lm_predictions,
+     main="Target Vs. new_lm_predictions")
+
+par(mfrow=c(1,1))
+scatter.smooth(new_lm_predictions)
+
+
+rmse <- sqrt(mean((lm_predictions - test_set$Price_Gross)^2))
+rmse
+
+mae <- mean(abs(lm_predictions - test_set$Price_Gross))
+mae
+
+
+
