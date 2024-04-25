cbb403c630aae594f296f0c3127890c8bbabca1e
cgleonr <carlos.leon@stud.hslu.ch>
2024-04-25 09:49:17 +0200

    Neural Network finally works, binary classifier -> 'High_Ticket' column.

--- scripts/03_5_NeuralNetwork.R
+++ scripts/03_5_NeuralNetwork.R
@@ -11,30 +11,54 @@ library(neuralnet)
 enc_data <- read_excel("data/data_cleaned/data_total_model.xlsx")
 enc_data <- enc_data[, !(names(enc_data) %in% "X"), drop = TRUE]
 
-# Declaring Categorical Variables
-enc_data$Canton_num <- factor(enc_data$Canton_num)
-enc_data$Customer_Segment_num <- factor(enc_data$Customer_Segment_num)
-enc_data$Category_num <- factor(enc_data$Category_num)
-enc_data$Nr_rooms <- factor(enc_data$Nr_rooms)
-enc_data$Package_Product_num <- factor(enc_data$Package_Product_num)
-
 #Feature Engineering
 enc_data$Price_per_m2 <- enc_data$Price_Gross / enc_data$Size_m2
 q3 <- median(enc_data$Price_per_m2) + IQR(enc_data$Price_per_m2) / 2
 enc_data$High_Ticket <- enc_data$Price_per_m2 > q3
 enc_data$High_Ticket <- ifelse(enc_data$High_Ticket, 1, 0)
 
-enc_data <- enc_data %>% mutate_if(is.character, as.factor) # Converting to factor w/
-enc_data$High_Ticket <- factor(enc_data$High_Ticket)
+# Declaring Categorical Variables
+enc_data$Canton_num <- as.numeric(as.factor(enc_data$Canton_num))
+enc_data$Customer_Segment_num <- as.numeric(as.factor(enc_data$Customer_Segment_num))
+enc_data$Category_num <- as.numeric(as.factor(enc_data$Category_num))
+enc_data$Nr_rooms <- as.numeric(as.factor(enc_data$Nr_rooms))
+enc_data$Package_Product_num <- as.numeric(as.factor(enc_data$Package_Product_num))
 
-#  2 levels
-#Splitting the data into training and testing
-set.seed(245)
+# Normalizing the data
+#minmax
+High_Ticket <- enc_data$High_Ticket
+features <- subset(enc_data, select = -High_Ticket)
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+maxmindf_features <- as.data.frame(lapply(features, normalize))
 
-data_rows <- floor(0.8 * nrow(enc_data))
-train_indices <- sample(c(1:nrow(enc_data)), data_rows)
-train_data <- enc_data[train_indices,]
-test_data <- enc_data[-train_indices,]
+maxmindf <- cbind(High_Ticket, maxmindf_features)
 
-################################################################################
+# Split into train and test sets
+set.seed(36)
+smpl <- sample.int(n=nrow(maxmindf), size = floor(0.8*nrow(maxmindf)),
+                   replace = FALSE)
+train_maxmin <- maxmindf[smpl,]
+test_maxmin <- maxmindf[-smpl,]
+
 # Modeling
+model_maxmin <- neuralnet(High_Ticket ~ ., train_maxmin, hidden = c(10, 10, 10), 
+                   linear.output = FALSE)
+#plot(model_maxmin)
+
+# Predicting
+pred_maxmin <- predict(model_maxmin, test_maxmin)
+model_maxmin$result.matrix
+
+# Testing the Accuracy of the models
+temp_test_maxmin <- subset(test_maxmin, select = -High_Ticket)
+maxmin_model.results <- compute(model_maxmin, temp_test_maxmin)
+maxmin_results <- data.frame(actual = test_maxmin$High_Ticket, prediction = maxmin_model.results$net.result)
+maxmin_results
+
+# Confusion matrix
+roundedresults<-sapply(maxmin_results,round,digits=0)
+roundedresultsdf=data.frame(roundedresults)
+attach(roundedresultsdf)
+table(actual,prediction)
