a9890f062d2d2a46d4ce361a9bb9a2fdf10df609
cgleonr <carlos.leon@stud.hslu.ch>
2024-04-25 17:15:43 +0200

    Started work on SVM. SVM will be used to compare binary classification ability vs Neural Network to propose one of the two as the better model for predicting high-ticket properties

--- scripts/03_6_SupportVectorMachine.R
+++ scripts/03_6_SupportVectorMachine.R
@@ -6,17 +6,39 @@
 # Required Packages
 library(readxl)
 library(caret)
+library(e1071)
 #-------------------------------------------------------------------------------
 
 # Reading in data
-enc_data <- read_excel("data/data_cleaned/data_total_model.xlsx")
-enc_data <- enc_data[, !(names(enc_data) %in% "X"), drop = TRUE]
+ohe_data <- read_excel("data/data_cleaned/data_total_model_one_hot_encoded.xlsx")
+ohe_data <- enc_data[, !(names(enc_data) %in% "X"), drop = TRUE]
 
 #Feature Engineering
-enc_data$Price_per_m2 <- enc_data$Price_Gross / enc_data$Size_m2
-q3 <- median(enc_data$Price_per_m2) + IQR(enc_data$Price_per_m2) / 2
-enc_data$High_Ticket <- enc_data$Price_per_m2 > q3
-enc_data$High_Ticket <- ifelse(enc_data$High_Ticket, 1, 0)
+ohe_data$Price_per_m2 <- ohe_data$Price_Gross / ohe_data$Size_m2
+q3 <- median(ohe_data$Price_per_m2) + IQR(ohe_data$Price_per_m2) / 2
+ohe_data$High_Ticket <- ohe_data$Price_per_m2 > q3
+ohe_data$High_Ticket <- ifelse(ohe_data$High_Ticket, 1, 0)
 
-str(enc_data)
+# Nr_rooms.8 has only one positive value, so we remove the column
+ohe_data <- ohe_data[, !names(ohe_data) %in% c("Price_per_m2","Nr_rooms.8")]
 
+# Random seed for reproducibility
+set.seed(32)
+
+# Split data into training and test sets
+split_index <- createDataPartition(ohe_data$High_Ticket, times = 5, p=.8)
+train_index <- split_index[[1]]
+test_index <- split_index[[2]]
+
+train_set <- ohe_data[train_index,]
+test_set <- ohe_data[test_index,]
+
+# Modelling
+svm_formula <- High_Ticket ~ .
+svm_model <- svm(svm_formula, data=train_set, kernel="linear", cost=1)
+
+
+# Predictions
+preds <- predict(svm_model, newdata = test_set[-test_set$High_Ticket])
+cm <- confusionMatrix(preds, test_set$High_Ticket)
+print(cm)
